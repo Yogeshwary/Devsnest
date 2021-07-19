29c06addd3f40270b60ab8fd6d8954288b6aabc9
Yogeshwary <86066751+Yogeshwary@users.noreply.github.com>
2021-07-19 23:27:04 +0530

    Update frontend9.js

--- Day9/frontend9.js
+++ Day9/frontend9.js
@@ -3,20 +3,20 @@ var booked = document.querySelector('.bookedSeat');
 var remaining = document.querySelector('.remainingSeat');
 booked.innerHTML = 0;
 remaining.innerHTML = 36;
-box[3].style.backgroundColor = 'red';
+//box[3].style.backgroundColor = 'red';
 
 window.onload = function() {
   ClickHandler();
 }
 
 function click(event) {
-  if(box[this.id].style.backgroundColor == "white"){ 
-    box[this.id].style.backgroundColor = "black";
+  if(box[this.id].style.backgroundColor === #ffffff){ 
+    box[this.id].style.backgroundColor = #000000;
     booked.innerHTML =++booked.innerHTML;
     remaining.innerHTML = remaining.innerHTML-1;    
   }
   else{ 
-    box[this.id].style.backgroundColor = "white";
+    box[this.id].style.backgroundColor = #ffffff;
     booked.innerHTML = --booked.innerHTML;
     remaining.innerHTML = Number(remaining.innerHTML)+1;
   }
