6ed654377d332687fdf71933d260d821ca6d35d8
Yogeshwary <86066751+Yogeshwary@users.noreply.github.com>
2021-07-19 21:28:32 +0530

    Add files via upload

---
+++ Day9/frontend9.js
@@ -0,0 +1,24 @@
+var box = document.getElementById("outerBox").querySelectorAll(".box");
+var booked = document.querySelector(".bookedSeat");
+var remaining = document.querySelector(".remainingSeat");
+booked.innerHTML = 0;
+remaining.innerHTML = 36;
+//box[2].style.backgroundColor = "red";
+
+//window.onload = function() {
+//	Click();
+//}
+
+function Click(){
+	for(var i=0; i<=box.length; i++){
+		console.log(i);
+		(box[i]).addEventListener('click', function(){
+			if(box[i].style.backgroundColor == "white"){ 
+				box[i].style.backgroundColor = "black";
+			}
+			else{ 
+				box[i].style.backgroundColor = "white";
+			}
+		})
+	}
+}
